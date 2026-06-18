ef6a60109afe382867f7ebd93902a17311377a0e
Reyn <86341393+StahlReyn@users.noreply.github.com>
2026-06-18 11:14:45 +0700

    Compose env, Validate workflow, Module Template

---
+++ addons/expense_network/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Expense Network",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Author Name",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    'installable': True,
+    'application': True,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
